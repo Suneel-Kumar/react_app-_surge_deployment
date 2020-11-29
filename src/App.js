ecd3d7fea8293601a87147d0b208e33e64233be9
Suneel Kumar <malhisuneel@gmail.com>
2020-11-29 21:27:36 +0500

    make_littleBit_changes_to_appfile

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>React App Deployment Surge. </p>
+        <p>React App Deployment Surge. <br /> Changes to react_project. </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
